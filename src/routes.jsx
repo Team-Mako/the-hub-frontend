f5ddde6caddbf2fa9d8c88516d0314734221facd
Leonart Gutz <leonartgutz@Leonarts-MacBook-Pro.local>
2020-06-10 21:57:30 -0700

    first commit

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import MainNav from './components/MainNav';
-import Home from './pages/Home';
-import About from './pages/About';
-import MainFooter from './components/MainFooter';
 
 function Routes() {
   return (
-    <BrowserRouter>
-      <MainNav />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about/" exact component={About} />
-      </Switch>
-      <MainFooter />
-    </BrowserRouter>
+    <BrowserRouter />
   );
 }
 
