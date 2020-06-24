91af07a0ee4a8183a707ea7e8049808b4164270f
Leonart Gutz <leonartgutz@Leonarts-MacBook-Pro.local>
2020-06-23 22:05:52 -0700

    Gustavo wireframe changes

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { RocketImg, RocketGp, BalloonImg, BalloonGp } from './Assets';
 
 const CallToAction = () => {
     return(
       <section className="call-to">
         <div className="call-to__inner">
 
-          <h2>Join The HUB community to <br /> share your do it yourself project.</h2>
+          <div className="call-to__left">
+            {/* <img src={ RocketImg } srcSet={ RocketGp } alt="The Hub Rocket" /> */}
+            <img src={ BalloonImg } srcSet={ BalloonGp } alt="The Hub Balloon" />
+          </div>
 
-          <p>Let’s make it! See other DIY projects that <br /> people love & participate yourself.</p>
+          <div className="call-to__right">
+            {/* <h2>Join The HUB community to <br /> share your do it yourself project.</h2>
 
-          <NavLink to="/">Sign Up</NavLink>
+            <p>Let’s make it! See other DIY projects that <br /> people love & participate yourself.</p>
+
+            <NavLink to="/">Sign Up</NavLink> */}
+
+            <h2>Engage The HUB community by <br /> sharing your thoughts.</h2>
+
+            <p>Let’s make it! Share your opinions on other <br /> people’s projects and give them feedback.</p>
+
+            <NavLink to="/">Check the projects</NavLink>
+          </div>
 
         </div>
       </section>
