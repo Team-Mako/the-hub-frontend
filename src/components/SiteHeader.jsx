0c4b95f49f5ee4340da50589bb4eb4833fa06b9d
Leonart Gutz <leonartgutz@Leonarts-MacBook-Pro.local>
2020-06-25 02:01:41 -0700

    CSS Changes and Small Fixies

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaChevronDown } from 'react-icons/fa';
-import { LogoRegular } from './Assets';
+import { LogoSmall } from './Assets';
 
 const Header = () => (
   <section className="site-header">
     <div className="site-header__inner">
-      <NavLink to="/" className="site-header__logo"><img src={LogoRegular} alt="The Hub Logo" /></NavLink>
+      <NavLink to="/" className="site-header__logo"><img src={LogoSmall} alt="The Hub Logo" /></NavLink>
 
-      <ul>
+      <ul className="site-header__menu">
         <li>
-          Categories
-          <FaChevronDown />
-          {/* <ul>
-              <li><NavLink to="/category/clothing">Clothing</NavLink></li>
-              <li><NavLink to="/category/cooking">Cooking</NavLink></li>
-              <li><NavLink to="/category/crafts">Crafts</NavLink></li>
-              <li><NavLink to="/category/drinks">Drinks</NavLink></li>
-              <li><NavLink to="/category/furniture">Furniture</NavLink></li>
-              <li><NavLink to="/category/music">Music</NavLink></li>
-              <li><NavLink to="/category/homedecor">Home Decor</NavLink></li>
-              <li><NavLink to="/category/tech">Tech</NavLink></li>
-              <li><NavLink to="/category/outdoors">Outdoors</NavLink></li>
-            </ul> */}
+          <NavLink to="/category">
+            Categories
+            <FaChevronDown />
+          </NavLink>
+          <ul className="site-header__submenu">
+            <li><NavLink to="/category/clothing">Clothing</NavLink></li>
+            <li><NavLink to="/category/cooking">Cooking</NavLink></li>
+            <li><NavLink to="/category/crafts">Crafts</NavLink></li>
+            <li><NavLink to="/category/drinks">Drinks</NavLink></li>
+            <li><NavLink to="/category/furniture">Furniture</NavLink></li>
+            <li><NavLink to="/category/music">Music</NavLink></li>
+            <li><NavLink to="/category/homedecor">Home Decor</NavLink></li>
+            <li><NavLink to="/category/tech">Tech</NavLink></li>
+            <li><NavLink to="/category/outdoors">Outdoors</NavLink></li>
+          </ul>
         </li>
         <li><NavLink to="/">About</NavLink></li>
         <li><NavLink to="/">Contact</NavLink></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { FaSearch, FaHeart, FaEye } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
-import { HeroImg } from '../components/Assets';
+import { HeroImg, PostCoverExample, AuthorImgExample } from '../components/Assets';
 
 class Home extends Component {
   constructor() {
@@ -16,23 +18,93 @@ class Home extends Component {
         <main className="home-main">
           <div className="home-main__inner">
 
-            <div className="home-main__hero">
+            <section className="hero-section">
 
-              <div className="home-main__left">
-                <h1>Discover your DIY for today</h1>
-                <p>
-                  The HUB is the best place to find & showcase any project that
-                  <span>you can do it yourself</span>
-                </p>
+              <div className="hero-section__left">
+                <h1>Discover your <br /> DIY for today</h1>
+
+                <p>The HUB is the best place to <br /> find & showcase any project<br /> that <strong>you can do it yourself</strong></p>
+
+                <form>
+                  <input type="search" placeholder="What do you want to create?" />
+                  <button type="button"><span aria-hidden="true" className="visually-hidden">Search Button</span><FaSearch /></button>
+                </form>
               </div>
 
-              <div className="home-main__right">
+              <div className="hero-section__right">
                 <img src={HeroImg} alt="The Hub Hero" />
               </div>
 
-            </div>
+            </section>
 
-            <div className="home-main__best-of" />
+            <section className="post-section">
+
+              <div className="post-section__inner">
+
+                <h2 className="home-main__title">Best of The HUB<br /> <span>This Week</span></h2>
+
+                <div className="post-section__list">
+                  <NavLink to="/" className="post-section__box">
+                    <img className="post-section__cover" src={PostCoverExample} alt="Img of a drink" />
+                    <h3>Seriously Good White Russian Cocktail Recipe</h3>
+
+                    <div className="post-section__details">
+
+                      <div className="post-section__author">
+                        <img src={AuthorImgExample} alt="Img of the post creator" />
+                        <p>Leona</p>
+                      </div>
+
+                      <div className="post-section__meta">
+                        <span><FaHeart /> 609</span>
+                        <span><FaEye /> 120</span>
+                      </div>
+
+                    </div>
+                  </NavLink>
+
+                  <NavLink to="/" className="post-section__box">
+                    <img className="post-section__cover" src={PostCoverExample} alt="Img of a drink" />
+                    <h3>Seriously Good White Russian Cocktail Recipe</h3>
+
+                    <div className="post-section__details">
+
+                      <div className="post-section__author">
+                        <img src={AuthorImgExample} alt="Img of the post creator" />
+                        <p>Leona</p>
+                      </div>
+
+                      <div className="post-section__meta">
+                        <span><FaHeart /> 609</span>
+                        <span><FaEye /> 120</span>
+                      </div>
+
+                    </div>
+                  </NavLink>
+
+                  <NavLink to="/" className="post-section__box">
+                    <img className="post-section__cover" src={PostCoverExample} alt="Img of a drink" />
+                    <h3>Seriously Good White Russian Cocktail Recipe</h3>
+
+                    <div className="post-section__details">
+
+                      <div className="post-section__author">
+                        <img src={AuthorImgExample} alt="Img of the post creator" />
+                        <p>Leona</p>
+                      </div>
+
+                      <div className="post-section__meta">
+                        <span><FaHeart /> 609</span>
+                        <span><FaEye /> 120</span>
+                      </div>
+
+                    </div>
+                  </NavLink>
+                </div>
+
+              </div>
+
+            </section>
 
             <div className="home-main__categories" />
 
