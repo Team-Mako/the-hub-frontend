064815e84b83eb6e3f536ac34c2539efbe63b19c
Leonart Gutz <leonartgutz@Leonarts-MacBook-Pro.local>
2020-07-03 10:26:15 -0700

    Real Files

--- src/pages/Home/PostSection.jsx
+++ src/pages/Home/PostSection.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { FaHeart, FaEye } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
+import MediaQuery from 'react-responsive';
 import api from '../../services/api';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 class PostSection extends Component {
   constructor(props) {
@@ -26,29 +29,58 @@ class PostSection extends Component {
 
           <h2 className="home-main__title">Best of The HUB<br /> <span>This Week</span></h2>
 
-          <div className="post-section__list">
-            { posts.map((post) => (
+          <MediaQuery maxDeviceWidth={420}>
+            <div className="post-section__slider">
+              <Carousel showThumbs={false} showIndicators={false} showStatus={false} infiniteLoop>
+                { posts.map((post) => (
 
-              <NavLink key={post.id} to="/" className="post-section__box">
-                <img className="post-section__cover" src={require(`../../assets/uploads/${post.post_cover}`)} alt="Img of a drink" />
-                <h3>{post.post_title}</h3>
+                  <NavLink key={post.id} to="/" className="post-section__box">
+                    <img className="post-section__cover" src={require(`../../assets/uploads/${post.post_cover}`)} alt="Img of a drink" />
+                    <h3>{post.post_title}</h3>
 
-                <div className="post-section__details">
+                    <div className="post-section__details">
 
-                  <div className="post-section__author">
-                    <img src={require(`../../assets/uploads/${post.user_avatar}`)} alt="Img of the post creator" />
-                    <p>{post.user_name}</p>
+                      <div className="post-section__author">
+                        <img src={require(`../../assets/uploads/${post.user_avatar}`)} alt="Img of the post creator" />
+                        <p>{post.user_name}</p>
+                      </div>
+
+                      <div className="post-section__meta">
+                        <span><FaHeart /> {post.post_like}</span>
+                        <span><FaEye /> {post.post_views}</span>
+                      </div>
+
+                    </div>
+                  </NavLink>
+                ))}
+              </Carousel>
+            </div>
+          </MediaQuery>
+
+          <MediaQuery minDeviceWidth={420}>
+            <div className="post-section__list">
+              { posts.map((post) => (
+                <NavLink key={post.id} to="/" className="post-section__box">
+                  <img className="post-section__cover" src={require(`../../assets/uploads/${post.post_cover}`)} alt="Img of a drink" />
+                  <h3>{post.post_title}</h3>
+
+                  <div className="post-section__details">
+
+                    <div className="post-section__author">
+                      <img src={require(`../../assets/uploads/${post.user_avatar}`)} alt="Img of the post creator" />
+                      <p>{post.user_name}</p>
+                    </div>
+
+                    <div className="post-section__meta">
+                      <span><FaHeart /> {post.post_like}</span>
+                      <span><FaEye /> {post.post_views}</span>
+                    </div>
+
                   </div>
-
-                  <div className="post-section__meta">
-                    <span><FaHeart /> {post.post_like}</span>
-                    <span><FaEye /> {post.post_views}</span>
-                  </div>
-
-                </div>
-              </NavLink>
-            ))}
-          </div>
+                </NavLink>
+              ))}
+            </div>
+          </MediaQuery>
 
         </div>
 
