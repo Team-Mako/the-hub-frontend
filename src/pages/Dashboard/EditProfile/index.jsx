6dc04d39c2a98b00b4f08b17595e0b67aca09e9d
Leonart Gutz <leonartgutz@hotmail.com>
2020-07-19 15:39:31 -0700

    Bug fixies #1

--- src/pages/Dashboard/EditProfile/index.jsx
+++ src/pages/Dashboard/EditProfile/index.jsx
@@ -1,67 +1,200 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { filesURL } from '../../../config/filesBucket';
 import { NoPic } from '../../../components/Assets';
+import api from '../../../services/api';
+import Alerts from '../../../components/Alerts';
 
-const MyProjects = ({ user }) => {
+const EditProfile = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [bio, setBio] = useState('');
+  const [alert, setAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [oldPassword, setOldPassword] = useState('');
+  const [avatar, setAvatar] = useState('');
+
+  const userData = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    function getUser() {
-      console.log('Test');
+    async function getUserData() {
+      if (localStorage.getItem('persist:thehub')) {
+        const getLocalStorage = JSON.parse(localStorage.getItem('persist:thehub'));
+        const userObj = JSON.parse(getLocalStorage.user);
+
+        const response = await api.get(`/show-user/${userObj.user_id}`);
+        setName(response.data[0].user_name);
+        setLastName(response.data[0].user_last_name);
+        setBio(response.data[0].user_bio);
+
+        if (response.data[0].user_avatar) {
+          setAvatar(`${filesURL}${response.data[0].user_avatar}`);
+        }
+      }
     }
 
-    getUser();
-  });
+    getUserData();
+  }, []);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handleBio = (e) => {
+    setBio(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleOldPassword = (e) => {
+    setOldPassword(e.target.value);
+  };
+
+  const timer = () => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 5100);
+  };
+
+  const handleAvatar = (e) => {
+    setAvatar(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const handleProfileSubmit = (e) => {
+    e.preventDefault();
+
+    api.put('/update-user', {
+      name,
+      lastName,
+      bio,
+    })
+      .then((res) => {
+        dispatch({ type: 'USER_UPDATE', payload: { user: { user_name: name } } });
+        setAlert(true);
+        setAlertMessage('Profile Updated!');
+        timer();
+      })
+      .catch((err) => {
+        setAlert(true);
+        setAlertMessage('Something went wrong, try again!');
+        timer();
+      });
+  };
+
+  const handlePasswordSubmit = (e) => {
+    e.preventDefault();
+
+    if (confirmPassword === password) {
+      api.put('/update-password', {
+        oldPassword,
+        password,
+      })
+        .then((res) => {
+          setAlert(true);
+          setAlertMessage('Password Changed!');
+          timer();
+        })
+        .catch((err) => {
+          setAlert(true);
+          setAlertMessage(err.response.data.error);
+          timer();
+        });
+    } else {
+      setAlert(true);
+      setAlertMessage('Password confirmation doesn\'t match');
+    }
+  };
+
+  const handleAvatarSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    formData.append('name', name);
+
+    api.put('/update-avatar', formData, { headers: { 'content-type': 'multipart/form-data' } })
+      .then((res) => {
+        dispatch({ type: 'AVATAR_UPDATE', payload: { user: { user_avatar: res.data.avatar } } });
+        setAlert(true);
+        setAlertMessage(res.data.message);
+        timer();
+      })
+      .catch((err) => {
+        setAlert(true);
+        setAlertMessage(err.response.data.error);
+        timer();
+      });
+  };
 
   return (
     <main className="dashboard">
-      <div className="dashboard__inner">
-        <section className="dashboard__profile">
-          <img src={user.user_avatar ? `${filesURL}${user.user_avatar}` : NoPic} alt="User profile avatar" />
-          <p>{user.user_name}</p>
-          <p>{user.user_email}</p>
+      <div className="dashboard__inner--profile">
+        <section className="forms__change-pic">
+          <form method="post" encType="multipart/form-data" onSubmit={handleAvatarSubmit}>
+            <label>
+              <figure>
+                <div className="avatar__img" style={{ background: `url(${avatar || NoPic})` }} />
+                <figcaption>Edit</figcaption>
+              </figure>
+              <input type="file" name="avatar" accept="image/*" onChange={handleAvatar} />
+            </label>
+
+            <p>{userData.user_name}</p>
+            <p>{userData.user_email}</p>
+
+            <button type="submit">Save</button>
+          </form>
         </section>
 
         <section className="dashboard__panel--alt">
           <h1 className="forms__profile-title">Account Information</h1>
-          <form className="forms__profile">
+          <form className="forms__profile" onSubmit={handleProfileSubmit}>
             <label>
               <p>Name</p>
-              <input type="text" />
+              <input type="text" value={name} onChange={handleName} />
             </label>
 
             <label>
               <p>Last Name</p>
-              <input type="text" />
-            </label>
-
-            <label>
-              <p>Email Address</p>
-              <input type="email" />
+              <input type="text" value={lastName} onChange={handleLastName} />
             </label>
 
             <label>
               <p>Bio</p>
-              <textarea />
+              <textarea onChange={handleBio} defaultValue={bio} />
             </label>
 
             <button type="submit">Save</button>
           </form>
 
           <h1 className="forms__profile-title">Password</h1>
-          <form className="forms__profile">
+          <form className="forms__profile" onSubmit={handlePasswordSubmit}>
             <label>
               <p>Old Password</p>
-              <input type="password" />
+              <input type="password" value={oldPassword} onChange={handleOldPassword} min="8" required />
             </label>
 
             <label>
               <p>New Password</p>
-              <input type="password" />
+              <input type="password" value={password} onChange={handlePassword} min="8" required />
             </label>
 
             <label>
               <p>Confirm Password</p>
-              <input type="password" />
+              <input type="password" value={confirmPassword} onChange={handleConfirmPassword} min="8" required />
             </label>
 
             <button type="submit">Change</button>
@@ -70,10 +203,9 @@ const MyProjects = ({ user }) => {
         </section>
 
       </div>
+      <Alerts active={alert} message={alertMessage} />
     </main>
   );
 };
 
-export default connect((state) => ({
-  user: state.user,
-}))(MyProjects);
+export default EditProfile;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Headers from './components/Headers';
 import SiteFooter from './components/SiteFooter';
 import Home from './pages/Home';
@@ -20,8 +21,12 @@ import AdminCategories from './pages/Admin/Categories';
 import AdminMaterials from './pages/Admin/Materials';
 import AdminCreateCategory from './pages/Admin/Categories/Create';
 import AdminCreateMaterial from './pages/Admin/Materials/Create';
+import api from './services/api';
 
 function Routes() {
+  const auth = useSelector((state) => state.auth);
+  api.defaults.headers.Authorization = `Bearer ${auth.token}`;
+
   return (
     <Switch>
       <Route path="/" exact>
