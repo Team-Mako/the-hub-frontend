bd82f6d09aced8effb2300988e2125d2148401b5
Leonart Gutz <leonartgutz@hotmail.com>
2020-07-21 20:47:21 -0700

    Update #10

--- src/config/filesBucket.js
+++ src/config/filesBucket.js
@@ -1,1 +1,2 @@
-export const filesURL = 'https://api.thehub.wmdd.ca/files/';
+export const filesURL = 'https://localhost:3333/files/';
+// export const filesURL = 'https://api.thehub.wmdd.ca/files/';
